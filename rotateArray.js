fcf9b6bf21e7c2fbbe463ecb6dfb10fa87a81013
Vladimir Yordanov <102076132+dodaev@users.noreply.github.com>
2022-10-02 12:02:48 +0300

    Add files via upload

---
+++ rotateArray.js
@@ -0,0 +1,23 @@
+function rotateArray(arr, rotation) {
+   let result = [];
+for (let index = 0; index < rotation; index++) {
+    result = arr.splice();
+     console.log(result);
+}
+
+
+      
+
+}
+rotateArray(['1',
+    '2',
+    '3',
+    '4'],
+    2);
+    console.log("-------------");
+    rotateArray(['Banana', 
+    'Orange', 
+    'Coconut', 
+    'Apple'], 
+    15);
+    
